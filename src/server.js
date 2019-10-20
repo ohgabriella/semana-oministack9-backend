ba2f0c42a992ebf036fcbc757417fe2ab052bd14
Gabriella Barreto <gabriella.gomes@conductor.com.br>
2019-10-20 14:35:22 -0300

    adicionando arquivos

---
+++ src/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose')
+const routes = require('./routes');
+const app = express();
+
+
+mongoose.connect('mongodb+srv://semanaomini:semanaomini@semanaomini-bhapk.mongodb.net/semana09?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+//req informações que o usuario manda
+//res informações de retorno da requisição
+
+//req.query = acessar query params (para filtros)
+//req.params = acessar route params (edição e delete)
+//req.body = acessar o corpo da requisição 
+
+app.use(express.json());
+app.use(routes); 
+
+app.listen(3333);
